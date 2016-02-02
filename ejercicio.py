3d31f660b892b6a01ebdc72031be68f6a4c95d5a
borja egea madrid <begea@zeta40.aulas.gsyc.es>
2016-02-02 12:47:00 +0100

    He hecho el ejercicio 13.4

--- ejercicio.py
+++ ejercicio.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+fd = open("/etc/passwd", "r")
+
+lineas = fd.readlines()
+fd.close()
+
+for linea in lineas:
+	trozos = linea.split(':')
+	usuario = trozos[0]
+	shell = trozos[-1]
+	print 'usuario:'+ usuario, 'shell:'+ shell,
+
+print '\nnumero de usuarios:', len(lineas)
 
